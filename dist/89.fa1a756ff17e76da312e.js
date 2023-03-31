b0ba9bc0a045c9b8568acb89f9f187d7479a84a0
Appokaliptik <alexanderstrannik@gmail.com>
2023-03-31 08:27:39 +0300

    fix stories

--- dist/89.fa1a756ff17e76da312e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkreact_middle=self.webpackChunkreact_middle||[]).push([[89],{8089:(e,t,r)=>{r.r(t),r.d(t,{default:()=>d});var a=r(5893),c=r(9887),u=r(3921);const d=function(){var e=(0,c.$G)("about").t;return(0,a.jsx)(u.T,{children:e("О сайте")})}}}]);
