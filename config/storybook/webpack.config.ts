75c0c80dd73970e21df86d0cb35cdf4e2523af47
Appokaliptik <alexanderstrannik@gmail.com>
2023-04-05 13:58:41 +0300

    fix test

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -9,8 +9,8 @@ export default ({ config }: { config: webpack.Configuration }) => {
     html: '',
     entry: '',
     src: path.resolve(__dirname, '..', '..', 'src'),
-    locales: path.resolve(__dirname, '..', '..', 'public'),
-    buildLocales: path.resolve(__dirname, '..', '..', 'public'),
+    locales: '',
+    buildLocales: '',
   };
 
   config.resolve?.modules?.push(paths.src);
